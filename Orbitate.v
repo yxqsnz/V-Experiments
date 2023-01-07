5cca13c69398f90cfb13e4629ceb1b9fa2cb1d45
yxqsnz <yxqsnz@gmail.com>
2023-01-07 08:05:57 -0300

    Orbitate: improve

--- Orbitate.v
+++ Orbitate.v
@@ -5,6 +5,11 @@ import gx
 import time
 import math
 
+const (
+	forcey = 200
+	forcex = 200
+)
+
 struct Game {
 mut:
 	gg               &gg.Context = unsafe { nil }
@@ -19,7 +24,7 @@ fn main() {
 	}
 
 	game.gg = gg.new_context(
-		bg_color: gx.rgb(174, 198, 255)
+		bg_color: gx.black
 		width: 600
 		height: 400
 		user_data: game
@@ -38,18 +43,31 @@ fn frame(mut game Game) {
 
 	speed := game.total_delta_time - game.delta_time
 
-	mut a, mut b := f64(size.width / 2), f64(size.height / 2)
-	a += 164 * math.sin(speed)
-	b += 100 * math.cos(speed) 
+	ca, cb := f64(size.width / 2), f64(size.height / 2)
+
+	mut a, mut b := ca, cb
+	mut a2, mut b2 := ca, cb
+
+	a += forcex * math.sin(speed)
+	b += forcey * math.cos(speed)
+	a2 += forcex * -math.sin(speed)
+	b2 += forcey * -math.cos(speed)
 
 	ctx.begin()
 
 	ctx.show_fps()
-	ctx.draw_circle_filled(f32(a), f32(b), 100, gx.red)
-	ctx.draw_text_def(0, 30, "Delta: ${game.delta_time}")
-	ctx.draw_text_def(0, 40, "Total Delta: ${game.total_delta_time}")
 
+	ctx.draw_circle_filled(f32(a2), f32(b2), 100, gx.white)
+	ctx.draw_circle_filled(f32(a), f32(b), 100, gx.pink)
 
+	cfg := gx.TextCfg{
+		color: gx.pink
+		size: 20
+	}
+
+	ctx.draw_text(0, 30, 'Delta: ${game.delta_time}', cfg)
+	ctx.draw_text(0, 50, 'Total Delta: ${game.total_delta_time}', cfg)
+	ctx.draw_text(0, 70, 'Pos (x, y; x, y): ${a} ${b}; ${a2} ${b2}', cfg)
 	ctx.end()
 
 	finished := sw.elapsed().seconds()
